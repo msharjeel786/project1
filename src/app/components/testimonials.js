41d5e5516ad05b68e617660b48513a945f6df60d
Sharjeel <muhammadsharjeel5680@gmail.com>
2026-02-17 12:43:09 +0500

    update

--- src/app/components/testimonials.js
+++ src/app/components/testimonials.js
@@ -21,7 +21,7 @@ export default function Testimonials() {
                                         "We saw a big boost in engagement and sales with their recommendation engine. It personalizes the shopping experience perfectly.
                                     </p>
                                     <div className="xb-item--author ul_li">
-                                        <div className="xb-item--avatar"><img src="/assets/img/avatar/img01.jpg" alt="avatar-image" /></div>
+                                        <div className="xb-item--avatar"><img src="/assets/img/avatar/avatar1.jpg" alt="avatar-image" /></div>
                                         <div className="xb-item--holder">
                                             <h3 className="xb-item--name">Riya Mehta</h3>
                                             <span className="xb-item--desig">Manager - Trendico</span>
@@ -42,7 +42,7 @@ export default function Testimonials() {
                                         "Their NLP tool saves us hours by extracting key info from large documents instantly. It's fast, accurate, and a huge time-saver."
                                     </p>
                                     <div className="xb-item--author ul_li">
-                                        <div className="xb-item--avatar"><img src="/assets/img/avatar/img02.jpg" alt="avatar-image" /></div>
+                                        <div className="xb-item--avatar"><img src="/assets/img/avatar/avatar2.jpg" alt="avatar-image" /></div>
                                         <div className="xb-item--holder">
                                             <h3 className="xb-item--name">Miguel Torres</h3>
                                             <span className="xb-item--desig">CEO & Funder - DocFlow</span>
@@ -63,7 +63,7 @@ export default function Testimonials() {
                                         "Their automation solutions saved us countless hours. Tasks that took days are now completed in minutes. The ROI was almost immediate."
                                     </p>
                                     <div className="xb-item--author ul_li">
-                                        <div className="xb-item--avatar"><img src="/assets/img/avatar/img03.jpg" alt="avatar-image" /></div>
+                                        <div className="xb-item--avatar"><img src="/assets/img/avatar/avatar3.jpg" alt="avatar-image" /></div>
                                         <div className="xb-item--holder">
                                             <h3 className="xb-item--name">Sebastian Clark</h3>
                                             <span className="xb-item--desig">Manager - SwiftLogix</span>
@@ -84,7 +84,7 @@ export default function Testimonials() {
                                         "The AI chatbot they developed has transformed our customer service. We now handle 70% of queries with higher customer satisfaction!"
                                     </p>
                                     <div className="xb-item--author ul_li">
-                                        <div className="xb-item--avatar"><img src="/assets/img/avatar/img04.jpg" alt="avatar-image" /></div>
+                                        <div className="xb-item--avatar"><img src="/assets/img/avatar/avatar4.jpg" alt="avatar-image" /></div>
                                         <div className="xb-item--holder">
                                             <h3 className="xb-item--name">Priya Ramirez</h3>
                                             <span className="xb-item--desig">CEO - BrightNest</span>
@@ -105,7 +105,7 @@ export default function Testimonials() {
                                         "We gained deep insights into customer behavior and performance. Their analytics made our decision-making sharper and more confident."
                                     </p>
                                     <div className="xb-item--author ul_li">
-                                        <div className="xb-item--avatar"><img src="/assets/img/avatar/img05.jpg" alt="avatar-image" /></div>
+                                        <div className="xb-item--avatar"><img src="/assets/img/avatar/avatar5.jpg" alt="avatar-image" /></div>
                                         <div className="xb-item--holder">
                                             <h3 className="xb-item--name">Sebastian Clark</h3>
                                             <span className="xb-item--desig">Director - Finexa Capital</span>
@@ -126,7 +126,7 @@ export default function Testimonials() {
                                         "Their automation solutions saved us countless hours. Tasks that took days are now completed in minutes. The ROI was almost immediate."
                                     </p>
                                     <div className="xb-item--author ul_li">
-                                        <div className="xb-item--avatar"><img src="/assets/img/avatar/img03.jpg" alt="avatar-image" /></div>
+                                        <div className="xb-item--avatar"><img src="/assets/img/avatar/avatar3.jpg" alt="avatar-image" /></div>
                                         <div className="xb-item--holder">
                                             <h3 className="xb-item--name">Sebastian Clark</h3>
                                             <span className="xb-item--desig">Manager - SwiftLogix</span>
